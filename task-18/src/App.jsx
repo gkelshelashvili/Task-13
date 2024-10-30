d459465af8de626aed6a908743269f80a67e77e9
gkelshelashvili <giorgikesho6@gmail.com>
2024-10-30 16:06:40 +0400

    change

--- task-18/src/App.jsx
+++ task-18/src/App.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import TodoList from './Components/TodoList'
 
 const API_KEY = 'D3TCuniFl2cmaWdst3ryPNNcD6yx_ZZCnaUiFW-uuhHptF1Wjw'
 
 const App = () => {
+  const [data,setData] = useState([])
   const onFormSubmit = (name) => {
     fetch('/api/v1/todo', {
       method: 'POST',
@@ -15,8 +16,21 @@ const App = () => {
     }).then(resp => console.log(resp)).catch((err) => console.log(err))
   }
 
+  useEffect(() => {
+    fetch('/api/v1/todo', {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${API_KEY}`
+      }
+    }).then((resp) => resp.json())
+      .then((data) => setData(data))
+  }, [])
+  
+  console.log(data.items)
   return (
     <div>
+      
       <TodoList onFormSubmit={onFormSubmit}/>
     </div>
   )
